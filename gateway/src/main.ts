3542a4a2f6bc833a1fd5673de8b42b2134e09c75
heryfitiavana22 <hheryfitiavana@gmail.com>
2024-08-16 15:12:12 +0300

    fix port listened gateway

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { RpcExceptionFilter } from './helper/rpc-exception.filter';
+import { env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalFilters(new RpcExceptionFilter());
-  await app.listen(3000);
+  await app.listen(env.GATEWAY_PORT);
 }
 bootstrap();
